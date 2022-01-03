d60aef460452c440a7a62dc3f21be95aff09dd30
David Savenmark <dsinemark@gmail.com>
2022-01-03 16:00:59 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OO_Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
